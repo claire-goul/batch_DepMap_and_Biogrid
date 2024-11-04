2652fc0b7ab9b6525c12d745cbe992ec4b4f53de
Claire Goul <66192018+claire-goul@users.noreply.github.com>
2024-11-04 07:15:46 -0800

    Create build.sh

---
+++ gene-network-app/backend/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# build.sh
+echo "Creating data directory..."
+mkdir -p data
+echo "Listing contents of current directory:"
+ls -la
+echo "Listing contents of data directory:"
+ls -la data/
