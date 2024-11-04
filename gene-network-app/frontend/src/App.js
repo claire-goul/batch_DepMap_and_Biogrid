86b1f1e08cbfe773e0a976cdaa25ef83f3b75505
Claire Goul <66192018+claire-goul@users.noreply.github.com>
2024-11-04 06:37:52 -0800

    Create App.js

---
+++ gene-network-app/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GeneNetworkVisualizer from './components/GeneNetworkVisualizer';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <GeneNetworkVisualizer />
+    </div>
+  );
+}
+
+export default App;
